8dc621b4c996f59bb0fd02e4ff01a0f0a93cc60e
falamhazam <32308293+falamhazam@users.noreply.github.com>
2017-11-08 22:00:59 +0100

    RAGS carousel init

--- RAGS/JS/main.js
+++ RAGS/JS/main.js
@@ -1,3 +1,12 @@
 $(document).ready(function () {
     $('[data-toggle="tooltip"]').tooltip();
 });
+
+
+var navbar = $('.navbar');
+console.log(navbar.innerHeight())
+
+var paddingTop = navbar.innerHeight();
+console.log(paddingTop);
+
+$('#all-container').css('margin-top', paddingTop);
